04da096bdb87d212cb6f7fcb44dba8fc64166abc
AdrienVictor <46904762+AdrienVictor@users.noreply.github.com>
2020-03-20 13:38:47 +0200

    uploading on github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SharedModule } from "./shared/shared.module";
+import { HomePageComponent } from "./home-page/home-page.component";
+
+import { environment } from "../environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDQ7JoXP9-gJvWIqAmRAStmZpqQFrKi5fs",
+    authDomain: "firestarter-8829b.firebaseapp.com",
+    databaseURL: "https://firestarter-8829b.firebaseio.com",
+    projectId: "firestarter-8829b",
+    storageBucket: "firestarter-8829b.appspot.com",
+    messagingSenderId: "289839936920",
+    appId: "1:289839936920:web:6ad86fc48f514bcd3fe7ed",
+    measurementId: "G-9N1N47E2BE"
+  }
 };
 
 /*
